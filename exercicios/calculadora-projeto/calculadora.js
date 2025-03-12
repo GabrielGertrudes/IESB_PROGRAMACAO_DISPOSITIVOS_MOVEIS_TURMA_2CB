3ccb554cd0d8c0cb0b5fe83698723bb3f7f41222
GabrielGertrudes <gabrielgertrudes151@gmail.com>
2025-03-11 22:38:41 -0300

    atividade 3

--- exercicios/calculadora-projeto/calculadora.js
+++ exercicios/calculadora-projeto/calculadora.js
@@ -1,1 +1,6 @@
-export const 
+export const operacoes = ["soma", "subtração", "multiplicação", "divisão"];
+
+export const soma = (a, b) => a + b;
+export const subtracao = (a, b) => a - b;
+export const multiplicacao = (a, b) => a * b;
+export const divisao = (a, b) => a / b;
