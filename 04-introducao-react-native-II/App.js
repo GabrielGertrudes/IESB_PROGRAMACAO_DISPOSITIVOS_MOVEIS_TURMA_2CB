e343e9e42f0bca7fbe7689a81d3542198bfe0bc8
GabrielGertrudes <gabrielgertrudes151@gmail.com]>
2025-03-21 21:42:14 -0300

    aula

--- 04-introducao-react-native-II/App.js
+++ 04-introducao-react-native-II/App.js
@@ -1,15 +1,22 @@
 // Imports
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  ScrollView,
+} from "react-native";
 
 // Função que representa o componente
 export default function App() {
   // Lógica do componente
-  const nome = "Gustavo"
+  const nome = "Gustavo";
 
   function alerta() {
-    alert("Cliclou no botão!!!!!")
+    alert("Cliclou no botão!!!!!");
   }
 
   // retorno dessa função com o template do que vai ser
@@ -24,50 +31,52 @@ export default function App() {
         {/*  */}
         {/* <StatusBar style="auto" /> */}
         {/* css inline */}
-        <Text style={{ fontSize: 50, fontStyle: 'italic' }} >Algum texto qualuer</Text>
+        <Text style={{ fontSize: 50, fontStyle: "italic" }}>
+          Algum texto qualuer
+        </Text>
 
         {/* css com StyleSheet */}
         <Text style={styles.textGrande}>HELLO</Text>
 
         <Text>{nome}</Text>
-        <Button title='Enviar' onPress={alerta}></Button>
+        <Button title="Enviar" onPress={alerta}></Button>
         {/* Imagem de fora com link */}
         <Image
           source={{
-            uri: 'https://i.pinimg.com/736x/47/e7/fd/47e7fd39f8e5a44877234f1d3c7bf1dc.jpg'
+            uri: "https://i.pinimg.com/736x/47/e7/fd/47e7fd39f8e5a44877234f1d3c7bf1dc.jpg",
           }}
           style={{
             height: 300,
-            width: 300
+            width: 300,
           }}
         />
         {/* Imagem de dentro do projeto */}
         <Image
-          source={require('./imagens/image.png')}
+          source={require("./imagens/image.png")}
           style={{
             height: 300,
-            width: 300
+            width: 300,
           }}
         />
         <Image
-          source={require('./imagens/image.png')}
+          source={require("./imagens/image.png")}
           style={{
             height: 300,
-            width: 300
+            width: 300,
           }}
         />
         <Image
-          source={require('./imagens/image.png')}
+          source={require("./imagens/image.png")}
           style={{
             height: 300,
-            width: 300
+            width: 300,
           }}
         />
         <Image
-          source={require('./imagens/image.png')}
+          source={require("./imagens/image.png")}
           style={{
             height: 300,
-            width: 300
+            width: 300,
           }}
         />
       </View>
@@ -79,12 +88,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
   textGrande: {
     fontSize: 40,
-    fontWeight: 900
-  }
+    fontWeight: 900,
+  },
 });
